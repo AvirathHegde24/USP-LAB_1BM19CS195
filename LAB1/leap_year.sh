b14d88681689c17defd0bad0ba14c38261255a59
AvirathHegde24 <71491103+AvirathHegde24@users.noreply.github.com>
2021-12-02 10:01:08 +0530

    Update leap_year.sh

--- LAB1/leap_year.sh
+++ LAB1/leap_year.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo "Enter the year"
-read year1
-if [ `expr $year1 % 4` -eq 0 ]
-then	
-	echo "Leap Year"
+echo -n "Enter year: "
+read y
+if [ `expr $y % 4` -eq 0 -a `expr $y % 100` -ne 0 -o `expr $y % 400` -eq 0 ]
+then
+echo "$y is leap year"
 else
-	echo "Not a Leap Year"
+echo "$y is not a leap year"
 fi
